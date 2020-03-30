9e7798ecefbc15c2a18cd268e90956f022211722
Temelius <teme989@gmail.com>
2020-03-30 09:56:49 +0300

    Add package Periytyminen and add task Henkilo

---
+++ src/Periytyminen/Henkilo.java
@@ -0,0 +1,27 @@
+package Periytyminen;
+
+public class Henkilo {
+	
+	private String nimi;
+	private String osoite;
+	
+	public static void main(String[] args) {
+		Henkilo ada = new Henkilo("Ada Lovelace", "Korsontie 1 03100 Vantaa");
+		Henkilo esko = new Henkilo("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
+
+		System.out.println(ada);
+		System.out.println(esko);
+
+	}
+	
+	public Henkilo(String nimi, String osoite) {
+		this.nimi = nimi;
+		this.osoite = osoite;
+	}
+	
+	@Override
+	public String toString() {
+		return this.nimi + "\n  " + this.osoite;
+	}
+
+}
